373e0cacbbac13b06c2236585e0d38d55a30325b
ashishkrishna1 <ashishkrishna@alumni.stanford.edu>
2021-03-03 14:49:56 -0800

    add SPM Package.swift

---
+++ Tests/InteractiveSideMenuTests/InteractiveSideMenuTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import InteractiveSideMenu
+
+final class InteractiveSideMenuTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(InteractiveSideMenu().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import InteractiveSideMenuTests
+
+var tests = [XCTestCaseEntry]()
+tests += InteractiveSideMenuTests.allTests()
+XCTMain(tests)
